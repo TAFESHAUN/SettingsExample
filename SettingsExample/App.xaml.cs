39506f612c41d3f1606b8ec84f670ab7e25a2d02
shaunstummer <7850615+shaunstummer@users.noreply.github.com>
2023-11-15 08:59:48 +1100

    Add project files.

---
+++ SettingsExample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SettingsExample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SettingsExample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SettingsExample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
